5ea975cd3ac2dbf3caaf464b92b0cc89af8cf7a3
dynamicdev4all <149931467+dynamicdev4all@users.noreply.github.com>
2025-10-09 15:12:55 +0530

    Add files via upload

---
+++ SEC-F-APP/src/main/java/com/app/helper/SendOTP.java
@@ -0,0 +1,64 @@
+package com.app.helper;
+
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.internet.MimeMessage;
+
+	public class SendOTP {
+	    
+
+	        // provide recipient's email ID
+	        String to = "admin@rdec.in";
+	        // provide sender's email ID
+	        String from = "piebytwo014@gmail.com";
+
+	        // provide Mailtrap's username
+	        final String username = "api";
+	        final String password = "9c8aee72e7e4ebdd64b57bf2df163e1e";
+
+	        // provide Mailtrap's host address
+	        String host = "live.smtp.mailtrap.io";
+
+	        // configure Mailtrap's SMTP details
+	        Properties props = new Properties();
+	        props.put("mail.smtp.auth", "true");
+	        props.put("mail.smtp.starttls.enable", "true");
+	        props.put("mail.smtp.host", host);
+	        props.put("mail.smtp.port", "587");
+
+	        // create the Session object
+	        Session session = Session.getInstance(props,
+	            new Authenticator() {
+	                @Override
+	                protected PasswordAuthentication getPasswordAuthentication() {
+	                    return new PasswordAuthentication(username, password);
+	                }
+	            });
+
+	        try {
+	            // create a MimeMessage object
+	            Message message = new MimeMessage(session);
+	            // set From email field
+	            message.setFrom(new InternetAddress(from));
+	            // set To email field
+	            message.setRecipient(Message.RecipientType.TO, new InternetAddress(to));
+	            // set email subject field
+	            message.setSubject("Hello from the Mailtrap team");
+	            // set the content of the email message
+	            message.setText("Enjoy sending emails from Jakarta Mail!");
+
+	            // send the email message
+	            Transport.send(message);
+
+	            System.out.println("Email Message Sent Successfully!");
+
+	        } catch (MessagingException e) {
+	            throw new RuntimeException(e);
+	        }
+	    }
+	
+}
---
+++ SEC-F-APP/src/main/java/com/app/servlets/LoginServlet.java
@@ -0,0 +1,57 @@
+package com.app.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String emailAdd = request.getParameter("email");
+		String password = request.getParameter("password");
+
+		System.out.println("Data rec from HTML page");
+		System.out.println("Your email add is :" +  emailAdd);
+		System.out.println("Your password is : "+password);
+		
+		if(emailAdd.equals("admin@rdec.in") && password.equals("123456")) {
+			HttpSession session = request.getSession();
+			session.setAttribute("name", "Deepak"); // data insert  or login
+			session.invalidate();
+//			response.sendRedirect("home_page.html");	
+			response.sendRedirect("home.jsp");	
+		}else {
+			System.out.println("Your email or password is incorrect");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
